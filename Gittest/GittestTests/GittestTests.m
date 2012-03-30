3d86338232cf31b9bfea393bd00e2ed20b5f6e5c
ifunbae <ifunbae@stcube.com>
2012-03-30 13:26:10 +0900

    asasasa

---
+++ Gittest/GittestTests/GittestTests.m
@@ -0,0 +1,32 @@
+//
+//  GittestTests.m
+//  GittestTests
+//
+//  Created by 배 연성 on 12. 3. 30..
+//  Copyright (c) 2012년 에스티큐브. All rights reserved.
+//
+
+#import "GittestTests.h"
+
+@implementation GittestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GittestTests");
+}
+
+@end
